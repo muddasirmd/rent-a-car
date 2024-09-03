2899de7a095f5c02f141c67f6f84b3e089506532
Muddasir Nisar <mnisar@teresol.com>
2024-09-03 16:43:35 +0500

    Added assets files.

--- client/src/components/logos_banner.vue
+++ client/src/components/logos_banner.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex flex-wrap gap-2">
-        <div v-for="(c, key) in companies" :key="key" class="flex flex-col gap-1 grow ">
+        <div v-for="(c, key) in companies" :key="key" class="flex flex-col items-center gap-1 grow ">
             
-            <img class="w-fit h-fit" :src="c.logo">
-            <h1 class="">{{ c.name }}</h1>
+            <img class="w-fit h-fit" :src="c.logo" alt="company-logo">
+            <h1 class="text-xs">{{ c.name }}</h1>
         </div>
     </div>
         
@@ -13,17 +13,23 @@
 
 let companies = [
     {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
-    {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
+    {name: "BMW", logo: new URL("@/assets/logos/bmw-logo.png", import.meta.url)},
+    {name: "Cadillac", logo: new URL("@/assets/logos/cadillac-logo.webp", import.meta.url)},
+    {name: "Chevrolet", logo: new URL("@/assets/logos/chevrolet-logo.webp", import.meta.url)},
+    {name: "Ferrari", logo: new URL("@/assets/logos/ferrari-logo.webp", import.meta.url)},
+    {name: "Ford", logo: new URL("@/assets/logos/ford-logo.webp", import.meta.url)},
+    {name: "Honda", logo: new URL("@/assets/logos/honda.webp", import.meta.url)},
+    {name: "Hyundai", logo: new URL("@/assets/logos/hyundai-logo.webp", import.meta.url)},
+    {name: "Kia", logo: new URL("@/assets/logos/kia-logo.webp", import.meta.url)},
+    {name: "Lamborghini", logo: new URL("@/assets/logos/lamborghini-logo.webp", import.meta.url)},
+    {name: "Mercedes", logo: new URL("@/assets/logos/Mercedes.webp", import.meta.url)},
+    {name: "Mitsubishi", logo: new URL("@/assets/logos/mitsubishi-logo.webp", import.meta.url)},
+    {name: "Nissan", logo: new URL("@/assets/logos/nissan-logo.webp", import.meta.url)},
+    {name: "Peugeot", logo: new URL("@/assets/logos/peugeot-logo.webp", import.meta.url)},
+    {name: "Suzuki", logo: new URL("@/assets/logos/suzuki-logo.webp", import.meta.url)},
+    {name: "Tesla", logo: new URL("@/assets/logos/tesla-logo.webp", import.meta.url)},
+    {name: "Toyota", logo: new URL("@/assets/logos/toyota-logo.webp", import.meta.url)},
+    {name: "Volkswagen", logo: new URL("@/assets/logos/volkswagen-logo.webp", import.meta.url)},
     ]
 </script>
 
--- client/src/layouts/header/header.vue
+++ client/src/layouts/header/header.vue
@@ -1,10 +1,10 @@
 <template>
     
-    <div class="flex justify-between items-center py-4 w-full h-24 border-b">
+    <div class="flex justify-between items-center py-4 w-full h-20 border-b">
         <div class="">
-            <img :src="logo">
+            <img :src="logo" alt="app-logo">
         </div>
-        <div class="flex h-full text-sm gap-5 px-4 items-center rounded-full border ">
+        <div class="flex h-full text-sm font-light gap-14 px-8 items-center rounded-full border cursor-pointer">
             <div>
                 <h1>Add Brand</h1>
             </div>
@@ -15,17 +15,23 @@
                 <h1>Add Price</h1>
             </div>
             <div>
-                <h1>Settings Icon</h1>
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 16 16" color="orange" style="color:orange" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M7 2v-0.25c0-0.413-0.338-0.75-0.75-0.75h-2.5c-0.413 0-0.75 0.337-0.75 0.75v0.25h-3v2h3v0.25c0 0.412 0.337 0.75 0.75 0.75h2.5c0.412 0 0.75-0.338 0.75-0.75v-0.25h9v-2h-9zM4 4v-2h2v2h-2zM13 6.75c0-0.412-0.338-0.75-0.75-0.75h-2.5c-0.412 0-0.75 0.338-0.75 0.75v0.25h-9v2h9v0.25c0 0.412 0.338 0.75 0.75 0.75h2.5c0.412 0 0.75-0.338 0.75-0.75v-0.25h3v-2h-3v-0.25zM10 9v-2h2v2h-2zM7 11.75c0-0.412-0.338-0.75-0.75-0.75h-2.5c-0.413 0-0.75 0.338-0.75 0.75v0.25h-3v2h3v0.25c0 0.412 0.337 0.75 0.75 0.75h2.5c0.412 0 0.75-0.338 0.75-0.75v-0.25h9v-2h-9v-0.25zM4 14v-2h2v2h-2z"></path>
+                </svg>
             </div>
         </div>
-        <div class="flex h-full items-center bg-blue-500">Button + Login/Signup</div>
+        <div class="flex h-full items-center">
+            <div class="px-4 py-3 rounded-full text-sm font-light text-white bg-orange-500">
+                <h1>Rent your car</h1>
+            </div>
+            
+        </div>
     </div>
         
 </template>
 
 <script setup>
 
-const logo = new URL("@/assets/logos/audi.webp", import.meta.url)
+const logo = new URL("@/assets/logos/car-logo.webp", import.meta.url)
 
 </script>
 
