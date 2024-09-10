bd18c66220989cd3d5178619767e1cdf227ff35e
Muddasir Nisar <mnisar@teresol.com>
2024-09-10 17:45:34 +0500

    Styled search modal brand and year dropdowns.

--- client/src/modals/searchModal.vue
+++ client/src/modals/searchModal.vue
@@ -1,10 +1,14 @@
 <template>
 
 <div v-if="showModal" class="absolute z-50 overflow-y-hidden flex items-center justify-center h-screen w-full bg-black bg-opacity-50">
-    <div class="flex flex-col p-6 w-[30rem] bg-white">
+    <div class="flex flex-col p-6 w-[30rem] rounded-lg bg-white">
         
-        <div class="flex border-b pb-5">
-            <h1 class="cursor-pointer" @click="closeModal">X</h1>
+        <div class="flex items-center border-b pb-5">
+            
+            <svg class="h-3 w-3 cursor-pointer fill-black" @click="closeModal" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" viewBox="0 0 490 490" xml:space="preserve">
+                <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490   489.292,457.678 277.331,245.004 489.292,32.337 "/>
+            </svg>
+            
             <h1 class="flex mx-auto text-xl font-semibold">Filter Your Search</h1>
         </div>
         
@@ -19,26 +23,34 @@
             </div>
             
             <div class="flex flex-col gap-2">
-                <label for="year" class="text-lg font-medium">Select Year</label>
-                <select id="year" name="year" class="h-auto border cursor-pointer text-base rounded-lg block w-full py-4 px-2 border-gray-300 text-gray-600 focus:outline-none">
-                    <option selected>Select year</option>
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                    <option value="2024">2024</option>
-                </select>
+                <label for="brand" class="text-lg font-medium">Select Year</label>
+                <div class="dropdown relative">
+                    <button @click="toggleYear = !toggleYear" ref="yearDropDownToggler" class="flex items-start border cursor-pointer text-base rounded-lg w-full py-3 px-2 border-gray-300 text-gray-600 focus:outline-none">
+                        Select Year
+                    </button>
+        
+                    <div v-if="toggleYear" ref="yearContainer" class="absolute w-full max-h-screen z-50 py-4 my-2 overflow-scroll rounded-md flex flex-col gap-2 bg-[#f9f9f9] dropdown-content">
+                        
+                        <div v-for="year,key in [2024,2023,2022,2021,2020,2019]" :key="key" class="flex p-3 items-center cursor-pointer hover:bg-[#f1f1f1]">
+                            
+                            
+                            <h1 class="font-light">{{ year }}</h1>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
             
             <div class="flex flex-col gap-2">
                 <label for="brand" class="text-lg font-medium">Select Brand</label>
                 <div class="dropdown relative">
-                    <button @click="toggleBrand = !toggleBrand" class="flex items-start border cursor-pointer text-base rounded-lg w-full py-3 px-2 border-gray-300 text-gray-600 focus:outline-none">
+                    <button @click="toggleBrand = !toggleBrand" ref="brandDropDownToggler" class="flex items-start border cursor-pointer text-base rounded-lg w-full py-3 px-2 border-gray-300 text-gray-600 focus:outline-none">
                         Select Brand
                     </button>
         
-                    <div v-if="toggleBrand" class="flex flex-col gap-2 max-h-screen overflow-auto dropdown-content">
+                    <div v-if="toggleBrand" ref="brandContainer" class="absolute w-full max-h-screen py-4 my-2 overflow-scroll rounded-md flex flex-col gap-2 bg-[#f9f9f9] dropdown-content">
                         
-                        <div v-for="brand,key in brandList" :key="key" class="flex gap-2 items-center">
+                        <div v-for="brand,key in brandList" :key="key" class="flex gap-2 p-3 items-center cursor-pointer hover:bg-[#f1f1f1]">
                             
                             <img :src="brand.logo" class="w-8" alt="brand-logo">
                             
@@ -48,6 +60,12 @@
                     </div>
                 </div>
             </div>
+
+            <div class="flex ">
+                <button class="flex w-full p-4 text-lg font-medium justify-center rounded-md text-white bg-[#ff6e31] hover:bg-orange-400">
+                    <span>Search</span>
+                </button>
+            </div>
         </form>
     </div>
 </div>
@@ -56,7 +74,7 @@
 
 <script>
 
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 // import Header from "@/layouts/header/header.vue"
 
 
@@ -65,6 +83,7 @@ export default{
     setup(){
 
         let toggleBrand = ref(false);
+        let toggleYear = ref(false);
 
         let brandList = [
             {name: "Audi", logo: new URL("@/assets/logos/audi.webp", import.meta.url)},
@@ -99,12 +118,50 @@ export default{
             document.body.classList.remove('no-scroll')
         }
 
+        const brandContainer = ref(null);
+        const brandDropDownToggler = ref(null);        
+        
+        const yearContainer = ref(null);
+        const yearDropDownToggler = ref(null);
+
+        onMounted(() => {
+            // Add a click event listener to the document when the component is mounted
+            document.addEventListener('click', handleClick);
+        });
+
+        function handleClick(event) {
+            handleBrandClickOutside(event);
+            handleYearClickOutside(event);
+        }
+
+
+        function handleBrandClickOutside(event){
+            
+            if(brandContainer.value && event.target != brandDropDownToggler.value && ![...brandContainer.value.children].includes(event.target)){
+                toggleBrand.value = false;
+            }
+        }        
+        
+        function handleYearClickOutside(event){
+            
+            if(yearContainer.value && event.target != yearDropDownToggler.value && ![...yearContainer.value.children].includes(event.target)){
+                toggleYear.value = false;
+            }
+        }
+
+
+
         return { 
             toggleBrand,
+            toggleYear,
             brandList,
             showModal,
             openModal,
             closeModal,
+            brandContainer,
+            brandDropDownToggler,
+            yearContainer,
+            yearDropDownToggler,
         }
     }
 }
