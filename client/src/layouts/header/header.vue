aaf0bd9a6986f9cde98ce8ef85072b022d4bac06
Muddasir Nisar <mnisar@teresol.com>
2024-10-02 17:38:47 +0500

    UI responsivesness

--- client/src/layouts/header/header.vue
+++ client/src/layouts/header/header.vue
@@ -6,34 +6,38 @@
             <img :src="logo" class="cursor-pointer" alt="app-logo">
         </router-link>
 
-        <div class="flex md:hidden cursor-pointer">
+        <div class="flex md:hidden cursor-pointer relative">
+
             <svg class="w-4 h-4 fill-orange-500" @click="toggleNav = !toggleNav" version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7 2v-0.25c0-0.413-0.338-0.75-0.75-0.75h-2.5c-0.413 0-0.75 0.337-0.75 0.75v0.25h-3v2h3v0.25c0 0.412 0.337 0.75 0.75 0.75h2.5c0.412 0 0.75-0.338 0.75-0.75v-0.25h9v-2h-9zM4 4v-2h2v2h-2zM13 6.75c0-0.412-0.338-0.75-0.75-0.75h-2.5c-0.412 0-0.75 0.338-0.75 0.75v0.25h-9v2h9v0.25c0 0.412 0.338 0.75 0.75 0.75h2.5c0.412 0 0.75-0.338 0.75-0.75v-0.25h3v-2h-3v-0.25zM10 9v-2h2v2h-2zM7 11.75c0-0.412-0.338-0.75-0.75-0.75h-2.5c-0.413 0-0.75 0.338-0.75 0.75v0.25h-3v2h3v0.25c0 0.412 0.337 0.75 0.75 0.75h2.5c0.412 0 0.75-0.338 0.75-0.75v-0.25h9v-2h-9v-0.25zM4 14v-2h2v2h-2z"></path>
             </svg>
-        </div>
 
-        <div v-if="toggleNav" class="flex md:hidden relative">
-            <div @click="showModal" class="absolute top-0 right-0 flex h-full text-sm font-light gap-10 px-8 items-center rounded-full border cursor-pointer hover:shadow-xl transition-shadow">
-                <div>
-                    <h1>Add Brand</h1>
-                </div>
-                <div>
-                    <h1>Model Year</h1>
-                </div>
-                <div>
-                    <h1>Add Price</h1>
-                </div>
-                <div>
-                    <svg class="w-4 h-4 fill-orange-500" version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7 2v-0.25c0-0.413-0.338-0.75-0.75-0.75h-2.5c-0.413 0-0.75 0.337-0.75 0.75v0.25h-3v2h3v0.25c0 0.412 0.337 0.75 0.75 0.75h2.5c0.412 0 0.75-0.338 0.75-0.75v-0.25h9v-2h-9zM4 4v-2h2v2h-2zM13 6.75c0-0.412-0.338-0.75-0.75-0.75h-2.5c-0.412 0-0.75 0.338-0.75 0.75v0.25h-9v2h9v0.25c0 0.412 0.338 0.75 0.75 0.75h2.5c0.412 0 0.75-0.338 0.75-0.75v-0.25h3v-2h-3v-0.25zM10 9v-2h2v2h-2zM7 11.75c0-0.412-0.338-0.75-0.75-0.75h-2.5c-0.413 0-0.75 0.338-0.75 0.75v0.25h-3v2h3v0.25c0 0.412 0.337 0.75 0.75 0.75h2.5c0.412 0 0.75-0.338 0.75-0.75v-0.25h9v-2h-9v-0.25zM4 14v-2h2v2h-2z"></path>
-                    </svg>
+            <div v-if="toggleNav" class="flex md:hidden justify-center absolute top-0 right-0 h-screen px-5 rounded-lg border bg-white">
+                
+                <div class="flex flex-col items-center justify-center gap-4 py-4">
+                    <div @click="showModal" class="flex h-12 text-xs font-light gap-6 px-8 items-center rounded-full border cursor-pointer hover:shadow-xl transition-shadow ">
+                        <div>
+                            <h1>Add Brand</h1>
+                        </div>
+                        <div>
+                            <h1>Model Year</h1>
+                        </div>
+                        <div>
+                            <h1>Add Price</h1>
+                        </div>
+                        
+                    </div>
+
+                    <div class="flex items-center w-full">
+                        <div class="flex justify-center w-full px-2 py-2 rounded-full text-xs font-light text-white bg-orange-500">
+                            <h1>Rent your car</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
 
-            <div class="hidden md:flex h-full items-center">
-                <div class="px-4 py-3 rounded-full text-sm font-light text-white bg-orange-500">
-                    <h1>Rent your car</h1>
-                </div>
-            </div>
         </div>
+
+        
 
         <div @click="showModal" class="hidden md:flex h-full text-sm font-light gap-14 px-8 items-center rounded-full border cursor-pointer hover:shadow-xl transition-shadow">
             <div>
